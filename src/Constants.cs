d58759fcfe21138d5205025d9f18cd54d30509c7
Đỗ Quốc Tuấn <tuandoquoc28@gmail.com>
2024-02-25 22:41:39 +0700

    feat: add blacklist

--- src/Constants.cs
+++ src/Constants.cs
@@ -12,4 +12,30 @@
         public static string CANNOT_FIND_PREV_SHEET = "Cannot find sheetId for the prev month";
         public static string CANNOT_FIND_SHEET_TODAY = "Today, do not support registration for lunch";
     }
+
+    public class CustomEqualityComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            // Implement your custom equality logic here
+            return x.Contains(y, StringComparison.OrdinalIgnoreCase) || y.Contains(x, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public int GetHashCode(string obj)
+        {
+            // Implement your custom hash code generation logic here
+            return obj.GetHashCode() ^ obj.GetHashCode();
+        }
+    }
+
+    public class BlackLists
+    {
+        public static HashSet<string> Set = new(new CustomEqualityComparer())
+        {
+            "Nguyễn Minh Thông",
+            "Nguyễn Văn Lạc",
+            "Huỳnh Lê Bảo",
+            "TTS"
+        };
+    }
 }
