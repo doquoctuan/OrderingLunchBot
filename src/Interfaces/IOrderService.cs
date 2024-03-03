a3d825b77aa02869b790d3ae48eb0e256f1c90f7
Đỗ Quốc Tuấn <tuandoquoc28@gmail.com>
2024-03-03 16:09:17 +0700

    feat: add auto block order ticket

--- src/Interfaces/IOrderService.cs
+++ src/Interfaces/IOrderService.cs
@@ -7,5 +7,6 @@
         Task<bool> Order(string userName, DateTime dateTime, bool isOrder = true, bool isAll = false);
         Task<Dictionary<int, (string, int)>> UnPaidList();
         Task<List<(string, string)>> CreateUnpaidImage();
+        Task BlockOrderTicket();
     }
 }
