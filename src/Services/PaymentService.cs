6f170cca27ed17b4e6e43e9661303b2708e42573
Đỗ Quốc Tuấn <tuandoquoc28@gmail.com>
2024-11-27 08:55:25 +0700

    Add XML docs and refactor GeneratePaymentLinkAsync
    Added XML documentation to PaymentService constructor and GeneratePaymentLinkAsync method, detailing their purposes, parameters, and return types. Refactored GeneratePaymentLinkAsync to store the generated URL in a local variable before returning it, improving code readability and maintainability.

--- src/Services/PaymentService.cs
+++ src/Services/PaymentService.cs
@@ -8,6 +8,12 @@ public class PaymentService : IPaymentService
     private readonly int _accountNumber;
     private readonly string _templateName;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PaymentService"/> class.
+    /// Sets the BIN code, account number, and template name from environment variables.
+    /// For getting BIN code, access to <see href="https://api.vietqr.io/v2/banks"/>.
+    /// If environment variables are not set, default values are used.
+    /// </summary>
     public PaymentService()
     {
         _binCode = int.Parse(Environment.GetEnvironmentVariable("BIN_CODE") ?? "970436");
@@ -15,8 +21,15 @@ public class PaymentService : IPaymentService
         _templateName = Environment.GetEnvironmentVariable("TEMPLATE_NAME") ?? "compact";
     }
 
+    /// <summary>
+    /// Generates a payment link with the specified amount and additional data.
+    /// </summary>
+    /// <param name="amount">The amount for the payment link.</param>
+    /// <param name="additionalData">Additional data to be included in the payment link.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the generated payment link as a string.</returns>
     public Task<string> GeneratePaymentLinkAsync(decimal amount, string additionalData)
     {
-        return Task.FromResult($"https://img.vietqr.io/image/{_binCode}-{_accountNumber}-{_templateName}.png?amount={amount:F0}&addInfo={additionalData}");
+        string url = $"https://img.vietqr.io/image/{_binCode}-{_accountNumber}-{_templateName}.png?amount={amount:F0}&addInfo={additionalData}";
+        return Task.FromResult(url);
     }
 }
